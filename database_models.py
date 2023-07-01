6276dbca739a1cc681b489829660e49b1226095f
Dariya Babu Malapati <117331227+dariyababumalapati@users.noreply.github.com>
2023-07-01 18:57:16 +0200

    created user_id functionality, created Users table clss, and introduced save() method to all the database classes.

--- database_models.py
+++ database_models.py
@@ -2,13 +2,32 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.String(32), unique=True, nullable=False)
+    # Add other columns as needed
+
+    def __init__(self, user_id):
+        self.user_id = user_id
+        # Initialize other columns as needed
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
 class Passage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.Text)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     flashcards = db.relationship('Flashcard', backref='passage', lazy=True)
 
-    def __init__(self, content):
+    def __init__(self, content, user_id):
         self.content = content
+        self.user_id = user_id
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
 
 
 class Flashcard(db.Model):
@@ -22,6 +41,10 @@ class Flashcard(db.Model):
         self.answer = answer
         self.passage_id = passage_id
 
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
 class Study(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.Text)
@@ -32,6 +55,11 @@ class Study(db.Model):
         self.question = question
         self.answer = answer
         self.passage_id = passage_id
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
 
 if __name__ == '__main__':
     print("database_models")
