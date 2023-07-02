a1c383595de679a3d70881c4ed7279154e22327a
Dariya Babu Malapati <117331227+dariyababumalapati@users.noreply.github.com>
2023-07-02 16:51:48 +0200

    Introduced get_title method iin the passageFlashcards class, new cloumn in the passage table, made some changes to PassageToCards class mainly because of the changes in the passage db class.

--- passage_class.py
+++ passage_class.py
@@ -2,15 +2,17 @@ from ai import PassageFlashcards
 
 from database_models import db, Passage, Study, Flashcard
 
+from database_actions import clear_study_table
+
 
 class PassageToCards:
 
-    def __init__(self, passage_data) -> None:
-        self.passage_db = Passage(passage_data)
+    def __init__(self, passage_data, user_id) -> None:
+        self.passage_data = passage_data
         self.passage_ai_obj = PassageFlashcards(passage_data)
+        self.user_id = user_id
+        self.num_flashcards = None  # Get the number of flashcards created
         self.passage_id = None
-        self.flashcard_transients = self.passage_ai_obj.generate_flashcards()
-        self.num_flashcards = len(self.flashcard_transients)  # Get the number of flashcards created
 
         
     def commit_passage_and_flashcards(self):
@@ -19,27 +21,25 @@ class PassageToCards:
         self._copy_flashcards_to_study()
     
     def _passage_to_database(self):
-        db.session.add(self.passage_db)
-        db.session.commit()
-        self.passage_id = self.passage_db.id
+        passage_title = self.passage_ai_obj.generate_title()
+        passage_transient = Passage(passage_content=self.passage_data, passage_title=passage_title, user_id= self.user_id)
+        passage_transient.save()
     
     def _flashcards_to_db(self):
-        for card in self.flashcard_transients:
+        flashcard_transients = self.passage_ai_obj.generate_flashcards()
+        for card in flashcard_transients:
             question = card['front']
             answer = card['back']
             flashcard = Flashcard(question=question, answer=answer, passage_id=self.passage_id)
-            db.session.add(flashcard)
-        db.session.commit()
+            flashcard.save()
 
     def _copy_flashcards_to_study(self):
-        Study.query.delete()
-        db.session.commit()
+        clear_study_table()
 
         flashcards = Flashcard.query.filter_by(passage_id=self.passage_id).all()
         for flashcard in flashcards:
             study_card = Study(question=flashcard.question, answer=flashcard.answer, passage_id=flashcard.passage_id)
-            db.session.add(study_card)
-        db.session.commit()
-    
+            study_card.save()
+
 if __name__ == 'main':
     print('passage_class module')
