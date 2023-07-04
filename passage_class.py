525ff653b5e8e30e950e212ae0f443ccb702f9d7
Dariya Babu Malapati <117331227+dariyababumalapati@users.noreply.github.com>
2023-07-04 23:19:14 +0200

    commiting the user to database is shifted to PassageToCards class.

--- passage_class.py
+++ passage_class.py
@@ -1,6 +1,6 @@
 from ai import PassageFlashcards
 
-from database_models import db, Passage, Study, Flashcard
+from database_models import User, Passage, Study, Flashcard
 
 from database_actions import clear_study_table
 
@@ -9,29 +9,35 @@ class PassageToCards:
 
     def __init__(self, passage_data, user_id) -> None:
         self.passage_data = passage_data
-        self.passage_ai_obj = PassageFlashcards(passage_data)
-        self.user_id = user_id
-        self.num_flashcards = None  # Get the number of flashcards created
+        self.user_table_obj = User(user_id)
         self.passage_id = None
+        self.num_flashcards = None  # Get the number of flashcards created
 
         
     def commit_passage_and_flashcards(self):
+        self._user_to_database()
         self._passage_to_database()
         self._flashcards_to_db()
         self._copy_flashcards_to_study()
+
+    def _user_to_database(self):
+        self.user_table_obj.save()
     
     def _passage_to_database(self):
         passage_title = self.passage_ai_obj.generate_title()
-        passage_transient = Passage(passage_content=self.passage_data, passage_title=passage_title, user_id= self.user_id)
+        passage_transient = Passage(passage_content=self.passage_data, passage_title=passage_title, user_id= self.user_table_obj.id)
         passage_transient.save()
+        self.passage_id = passage_transient.id
     
     def _flashcards_to_db(self):
-        flashcard_transients = self.passage_ai_obj.generate_flashcards()
+        passage_ai_obj = PassageFlashcards(self.passage_data)
+        flashcard_transients = passage_ai_obj.generate_flashcards()
         for card in flashcard_transients:
             question = card['front']
             answer = card['back']
             flashcard = Flashcard(question=question, answer=answer, passage_id=self.passage_id)
             flashcard.save()
+        self.num_flashcards = len(flashcard_transients)
 
     def _copy_flashcards_to_study(self):
         clear_study_table()
